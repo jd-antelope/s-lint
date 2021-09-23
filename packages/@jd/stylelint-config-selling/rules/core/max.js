84d1811345642d7332fd40aa8b01cb3b43318750
huozhongyi <huozhongyi@jd.com>
2021-09-23 21:47:19 +0800

    feat: add max and media stylelint

---
+++ packages/@jd/stylelint-config-selling/rules/core/max.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  // 限制允许的嵌套深度
+  "max-nesting-depth": [4, {"message": "限制相邻空行的数量：4"}],
+  // 限制相邻空行的数量
+  "max-empty-lines": [4, {"message": "限制相邻空行的数量：4"}],
+  // 限制行的长度
+  "max-line-length": [30, {"message": "限制行的长度：30"}]
+};
