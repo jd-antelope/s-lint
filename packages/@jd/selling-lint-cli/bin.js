7dae3f851bf4bc88b192df142dc78d62487c6fcf
zhengxiuyue <zhengxiuyue@jd.com>
2021-10-18 20:03:39 +0800

    fix

--- packages/@jd/selling-lint-cli/bin.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node   
-require('./build')
-
-// require('ts-node/register')
-// require('./src/index.ts')
--- packages/@jd/selling-lint-cli/src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-/**exports*/
-export * from './consts.js'
-
-export * from './spinner.js'
-
-export * from './logger.js'
-
-export * from './file.js'
